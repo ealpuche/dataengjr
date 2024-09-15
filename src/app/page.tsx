35dd9e6bc850ab522faa81a9bb322df7aba27579
Elmer Alexis Alpuche Morales <elmer.alpuche@gmail.com>
2024-09-14 18:29:45 -0600

    Init proyect with Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a dataengjr</h1>
+      <p>Este es mi sitio personal donde encontrarás información sobre mis proyectos, blogs, y más.</p>
+      <ul>
+        <li><a href="https://github.com/tu-usuario">GitHub</a></li>
+        <li><a href="https://youtube.com/tu-canal">YouTube</a></li>
+      </ul>
+    </div>
+  );
+}
